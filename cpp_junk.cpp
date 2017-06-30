9750ca1f577d64b2fe5f58be57a277eb4908ede8
Stan Drozd <drozdziak1@gmail.com>
2017-06-30 18:57:28 +0200

    Add a simple letter counter

--- cpp_junk.cpp
+++ cpp_junk.cpp
@@ -1,17 +1,44 @@
-// A simple program that computes the square root of a number
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-int main (int argc, char *argv[])
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#include <cstdio>
+
+using namespace std;
+
+struct FreqCmp {
+	bool operator()(auto &lhs, auto &rhs)
+	{
+		return lhs.second < rhs.second;
+	}
+};
+
+int main(int argc, char *argv[])
 {
-  if (argc < 2)
-    {
-    fprintf(stdout,"Usage: %s number\n",argv[0]);
-    return 1;
-    }
-  double inputValue = atof(argv[1]);
-  double outputValue = sqrt(inputValue);
-  fprintf(stdout,"The square root of %g is %g\n",
-          inputValue, outputValue);
-  return 0;
+	string input = (argc == 2) ? argv[1] : "The quick brown fox jumps over the lazy dog";
+	transform(input.begin(), input.end(), input.begin(), ::tolower);
+
+	vector<pair<char, int> > appearances;
+
+	for (int i = 0; i < 256; ++i)
+		appearances.push_back({i, 0});
+
+
+	for (const char &chr : input)
+		appearances[chr].second++;
+
+	// Sort in reverse for easy resize
+	sort(appearances.rbegin(), appearances.rend(), FreqCmp());
+
+	// Get only top 10 characters
+	appearances.resize(10);
+
+	for (auto i = appearances.begin(); i != appearances.end(); ++i) {
+		if (i->second == 0)
+			break;
+
+		printf("'%c'\t(%d):\t%zu\n", i->first, i->first, i->second);
+	}
+
+	return 0;
 }
