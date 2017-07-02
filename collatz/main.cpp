7ea17aed1a6c94793c89e696d10863bb17b3a271
Stan Drozd <drozdziak1@gmail.com>
2017-07-02 18:13:55 +0200

    update CMakeLists.txt, work on collatz

--- collatz/main.cpp
+++ collatz/main.cpp
@@ -1,5 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
 int main(int argc, char *argv[])
 {
-	
+	int input;
+
+	if (argc != 2 ) {
+		cerr << "Wrong input, using number 129";
+		input = 129;
+	} else {
+		input = stoi(argv[1]);
+	}
+
+	while (input != 1) {
+
+	}
 	return 0;
 }
