36e4b9e1b8be72f280302f8730ebd76f8ec21693
Stan Drozd <drozdziak1@gmail.com>
2017-07-02 19:24:16 +0200

    collatz: finish up

--- collatz/main.cpp
+++ collatz/main.cpp
@@ -4,17 +4,30 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	int input;
+	int num;
 
 	if (argc != 2 ) {
-		cerr << "Wrong input, using number 129";
-		input = 129;
+		cerr << "Wrong input, using number 129" << endl;
+		num = 129;
 	} else {
-		input = stoi(argv[1]);
+		try {
+			num = stoi(argv[1]);
+		} catch (...) {
+			cerr << "Couldn't parse your number, using 129" << endl;
+		}
 	}
 
-	while (input != 1) {
-
+	int i = 0;
+	while (num > 1) {
+		cout << "Step\t" << i << ":\t" << num << endl;
+		if (num % 2) {
+			num = 3 * num + 1;
+		} else {
+			num /= 2;
+		}
+		++i;
 	}
+	cout << "Step\t" << i << ":\t" << num << endl;
+	cout << "Took " << i << " steps, exiting..." << endl;
 	return 0;
 }
