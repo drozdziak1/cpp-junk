1cd8dd840f3a3b682548b1f6dbd45e549ff85238
Stan Drozd <drozdziak1@gmail.com>
2017-07-11 15:10:43 +0200

    chat: start working on a parameter binding example from Asio

--- chat/server/main.cpp
+++ chat/server/main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
+
+void print(const boost::system::error_code &e, boost::asio::deadline_timer& t, int& count)
+{
+	std::cout << "A timer thing No. " << count++ << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
 	boost::asio::io_service io;
 
-	boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
-	t.wait();
+	boost::asio::deadline_timer t1(io, boost::posix_time::seconds(1));
+	boost::asio::deadline_timer t2(io, boost::posix_time::seconds(2));
+	t1.async_wait(&print);
+	std::cout << "Between" << std::endl;
+	t2.async_wait(&print);
 
-	std::cout << "Hello, world!" << std::endl;
+	io.run();
+
+	std::cout << "Meantime stuff" << std::endl;
 
 	return 0;
 }
