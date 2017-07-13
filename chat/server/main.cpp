e83dad0eac88856435f6cc6cbc77541b3748c700
Stan Drozd <drozdziak1@gmail.com>
2017-07-13 20:59:10 +0200

    chat: copy the daytime ASIO tutorial example

--- chat/server/main.cpp
+++ chat/server/main.cpp
@@ -1,67 +1,45 @@
+#include <ctime>
 #include <iostream>
-
+#include <string>
 #include <boost/asio.hpp>
-#include <boost/bind.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
-#include <boost/thread/thread.hpp>
 
-class Printer
+using boost::asio::ip::tcp;
+
+std::string make_daytime_string()
 {
-public:
-	Printer(boost::asio::io_service &io, int count)
-		: strand_(io),
-		  timer1_(io, boost::posix_time::seconds(1)),
-		  timer2_(io, boost::posix_time::seconds(1)),
-		  count_(count)
-	{
-		timer1_.async_wait(boost::bind(&Printer::print1, this));
-		timer2_.async_wait(boost::bind(&Printer::print2, this));
-	}
-
-	~Printer()
-	{
-		std::cout << "Printer done." << std::endl;
-	}
-
-	void print1()
-	{
-		if (count_ > 0) {
-			std::cout << "Timer 1: " << --count_ << " executions left" << std::endl;
-
-			timer1_.expires_at(timer1_.expires_at() + boost::posix_time::seconds(1));
-			timer1_.async_wait(strand_.wrap(boost::bind(&Printer::print1, this)));
-		}
-	}
-
-	void print2()
-	{
-		if (count_ > 0) {
-			std::cout << "Timer 2: " << --count_ << " executions left" << std::endl;
-
-			timer2_.expires_at(timer2_.expires_at() + boost::posix_time::seconds(1));
-			timer2_.async_wait(strand_.wrap(boost::bind(&Printer::print2, this)));
-		}
-	}
-
-private:
-	boost::asio::io_service::strand strand_;
-	boost::asio::deadline_timer timer1_;
-	boost::asio::deadline_timer timer2_;
-	int count_;
-};
+	using namespace std;
+	time_t now = time(0);
+	std::cout << "Got a request!" << std::endl;
+	return ctime(&now);
+}
 
 int main(int argc, char *argv[])
 {
-	boost::asio::io_service io;
+	if (argc != 2) {
+		std::cerr << "Usage: chatserv <port>" << std::endl;
+		return 1;
+	}
+	try {
+		boost::asio::io_service io;
 
-	Printer p(io, 10);
-	boost::thread t(boost::bind(&boost::asio::io_service::run, &io));
+		std::cout << "Using port " << argv[1] << std::endl;
+		// Use port 13
+		tcp::acceptor acceptor(
+		    io,
+		    tcp::endpoint(tcp::v4(), std::stoi(argv[1]))
+		);
 
-	std::cout << "Before io.run()" << std::endl;
-	io.run();
-	std::cout << "After io.run()" << std::endl;
-	t.join();
+		for (;;) {
+			tcp::socket socket(io);
+			acceptor.accept(socket);
 
+			std::string message = make_daytime_string();
 
+			boost::system::error_code ignored_error;
+			boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+		}
+	} catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
 	return 0;
 }
