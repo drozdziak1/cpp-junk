08a8fbdf9f2eff4610a4c2c640af7e6d151c33f5
Stan Drozd <drozdziak1@gmail.com>
2017-07-22 00:19:34 +0200

    bigfac: lowercase a message command

--- bigfac/cmake-modules/FindGMP.cmake
+++ bigfac/cmake-modules/FindGMP.cmake
@@ -3,7 +3,7 @@
 find_path(GMP_INCLUDE_DIR NAMES gmp.h gmpxx.h )
 find_library(GMP_LIBRARIES NAMES gmp libgmp )
 find_library(GMPXX_LIBRARIES NAMES gmpxx libgmpxx )
-MESSAGE(STATUS "Found GMP: " ${GMP_LIBRARIES} " " ${GMPXX_LIBRARIES} )
+message(STATUS "Found GMP: " ${GMP_LIBRARIES} " " ${GMPXX_LIBRARIES} )
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP DEFAULT_MSG GMP_INCLUDE_DIR GMP_LIBRARIES)
