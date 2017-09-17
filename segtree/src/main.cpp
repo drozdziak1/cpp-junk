e89e13a7edbab6c20654da5f82b21c0a20e527df
Stan Drozd <drozdziak1@gmail.com>
2017-09-18 00:47:55 +0200

    segtree: finish a basic version of the algorithm

--- segtree/src/main.cpp
+++ segtree/src/main.cpp
@@ -1,43 +1,144 @@
-#include <SegTree.h>
-
 #include <climits>
-#include <cstdlib>
-
-#include <algorithm>
-#include <functional>
-#include <iostream>
-#include <iterator>
-#include <random>
+#include <cmath>
+#include <cstdio>
 #include <vector>
+#include <iostream>
+#include <algorithm>
 
-auto random_init(void)
+#define CHLD_L(i) (2 * (i) + 1)
+#define CHLD_R(i) (2 * (i) + 2)
+
+class SegTree
 {
-	// Create an RNG instance
-	std::random_device rnd_device;
+public:
+	SegTree(unsigned long long n);
+	~SegTree();
+	void increment (unsigned long long i, unsigned long long a, unsigned long long b, unsigned long long val);
+	unsigned long long  maximum(void);
+	unsigned long long maximum(unsigned long long a, unsigned long long b);
 
-	// Choose the Mersenne twister
-	std::mt19937 mersenne_engine(rnd_device());
-	std::uniform_int_distribution<int> dist(INT_MIN, INT_MAX);
+private:
+	unsigned long long *lows, *highs, *maxes, *deltas;
+	unsigned long long element_count;
 
-	return std::bind(dist, mersenne_engine);
+	unsigned long long maximum(unsigned long long i, unsigned long long a, unsigned long long b);
+	void init(unsigned long long i, unsigned long long a, unsigned long long b);
+	void prop(unsigned long long i);
+	void update(unsigned long long i);
+};
+
+SegTree::SegTree(unsigned long long n) :
+	element_count(n)
+{
+	lows = new unsigned long long[4 * n + 1];
+	highs = new unsigned long long[4 * n + 1];
+	maxes = new unsigned long long[4 * n + 1];
+	deltas = new unsigned long long[4 * n + 1];
+
+	init(0, 0, n - 1);
 }
 
-int main(int argc, char *argv[])
+SegTree::~SegTree()
 {
-	int n; // No. of elements
+	delete[] lows;
+	delete[] highs;
+	delete[] maxes;
+	delete[] deltas;
+}
 
-	if (argc != 2) {
-		std::cout << "How many elements? ";
-		std::cin >> n;
-	} else {
-		n = std::stoi(argv[1]);
+void SegTree::prop(unsigned long long i)
+{
+	// Propagate delta to the child nodes
+	deltas[CHLD_L(i)] += deltas[i];
+	deltas[CHLD_R(i)] += deltas[i];
+
+	// Zero out the parent
+	deltas[i] = 0;
+}
+
+void SegTree::update(unsigned long long i)
+{
+	unsigned long long left_sum = maxes[CHLD_L(i)] + deltas[CHLD_L(i)];
+	unsigned long long right_sum = maxes[CHLD_R(i)] + deltas[CHLD_R(i)];
+
+	maxes[i] = (left_sum > right_sum) ? left_sum : right_sum;
+}
+
+void SegTree::increment(unsigned long long i, unsigned long long a, unsigned long long b, unsigned long long val)
+{
+	if (b < lows[i] || highs[i] < a)
+		return;
+
+	if (a <= lows[i] && highs[i] <= b) {
+		deltas[i] += val;
+		return;
 	}
 
-	std::cout << "Choosing " << n << " random elements: " << std::endl;
+	prop(i);
+
+	increment(CHLD_L(i), a, b, val);
+	increment(CHLD_R(i), a, b, val);
+
+	update(i);
+}
+
+unsigned long long SegTree::maximum(void)
+{
+	return maximum(0, 0, element_count - 1);
+}
+
+unsigned long long SegTree::maximum(unsigned long long a, unsigned long long b)
+{
+	return maximum(0, a, b);
+}
+
+unsigned long long SegTree::maximum(unsigned long long i, unsigned long long a, unsigned long long b)
+{
+	if (b < lows[i] || highs[i] < a)
+		return 0;
+
+	if (a <= lows[i] && highs[i] <= b)
+		return maxes[i] + deltas[i];
+
+	prop(i);
+
+	unsigned long long max_left = maximum(CHLD_L(i), a, b);
+	unsigned long long max_right = maximum(CHLD_R(i), a, b);
+
+	update(i);
+
+	return (max_left > max_right) ? max_left : max_right;
+}
+
+void SegTree::init(unsigned long long i, unsigned long long a, unsigned long long b)
+{
+	lows[i] = a;
+	highs[i] = b;
+
+	if (a == b)
+		return;
+
+	unsigned long long m = (a + b) / 2;
+
+	init(2 * i + 1, a, m);
+	init(2 * i + 2, m + 1, b);
+}
 
 
-	std::vector<int> vec(n);
-	std::generate(begin(vec), end(vec), random_init());
+int main()
+{
 
+	unsigned long long k;
+	unsigned long a, b, n, m;
+	std::cin >> n >> m;
+	SegTree tree(n);
+
+	for (unsigned long long i = 0; i < m; ++i) {
+		std::cin >> a >> b >> k;
+
+		tree.increment(0, a - 1, b - 1, k);
+	}
+
+	std::cout << tree.maximum();
 	return 0;
 }
