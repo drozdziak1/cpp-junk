64070529cc48fdd6ffa2834ce71e4316ea4ea68c
Stan Drozd <drozdziak1@gmail.com>
2017-09-23 17:49:24 +0200

    rabinkarp: finish up, change example input to lorem ipsum

--- rabinkarp/src/main.cpp
+++ rabinkarp/src/main.cpp
@@ -17,7 +17,7 @@ int hash(std::string str)
 	return ret;
 }
 
-int hash_step(int hash, int len, char old_char, char new_char)
+void hash_step(int &hash, size_t len, char old_char, char new_char)
 {
 	int new_char_pow = 1;
 
@@ -32,24 +32,23 @@ int hash_step(int hash, int len, char old_char, char new_char)
 		new_char_pow *= PRIME;
 
 	// Attach the new character
-	return hash + new_char * new_char_pow;
+	hash += new_char * new_char_pow;
 }
 
 int main(int argc, char *argv[])
 {
-	char *current_chunk;
-	int phrase_hash;
+	int phrase_hash, current_hash, fake_matches = 0, real_matches = 0;
 	size_t file_length;
 
-	std::string phrase, filename;
+	std::string phrase, filename, current_chunk;
 	std::ifstream file;
 
 	if (argc != 3) {
 		std::cout << "Wrong number of arguments, using defaults"
 		          << std::endl;
 
-		phrase = "dog";
-		filename = "rabinkarp/input.txt";
+		filename = "rabinkarp/input.txt"; // Lorem ipsum inside
+		phrase = "turp"; // Got one collision
 	} else {
 		filename = argv[1];
 		phrase = argv[2];
@@ -70,9 +69,8 @@ int main(int argc, char *argv[])
 	file.seekg(0, std::ios_base::beg);
 
 	if (phrase.length() > file_length) {
-		std::cerr << "Error: Your phrase is bigger than the file!"
+		std::cerr << "Warning: Your phrase is bigger than the file!"
 		          << std::endl;
-		return EINVAL;
 	}
 
 	std::cout << "Looking for \"" << phrase << "\" in " << filename << "..."
@@ -82,22 +80,57 @@ int main(int argc, char *argv[])
 
 	std::cout << "Phrase Hash: " << phrase_hash << std::endl;
 
-	current_chunk = new char[phrase.length() + 1];
+	current_chunk.resize(phrase.length());
 
-	if (!current_chunk) {
-		std::cerr << "Failed to allocate the phrase comparison buffer"
-		          << std::endl;
-		file.close();
-		return 1;
-	}
+	file.read(&current_chunk.front(), phrase.length());
 
-	file.read(current_chunk, phrase.length());
-	current_chunk[phrase.length()] = '\0';
+	current_hash = hash(current_chunk);
+	std::cout << "Starting Chunk: \"" << current_chunk << "\"\n"
+	          << "Starting Chunk Hash: " << current_hash << '\n'
+	          << std::endl;
 
-	std::cout << "Current Chunk: \"" << current_chunk << "\"\n"
-	       << "Current Hash: " << hash(std::string(current_chunk)) << std::endl;
+	// End of boilerplate
+	do {
+		char old_char, new_char;
 
-	delete[] current_chunk;
+		// Do the cheap hash check
+		if (current_hash == phrase_hash) {
+
+			std::cout << "Got same hash " << current_hash
+			          << " from \"" << phrase
+			          << "\" and \""
+			          << current_chunk << "\" at char "
+			          << ((size_t) file.tellg() - phrase.length() + 1)
+			          << std::endl;
+
+			// Fall back to the more expensive string comparison
+			if (current_chunk == phrase) {
+				++real_matches;
+				std::cout << "It's a MATCH" << std::endl;
+			} else {
+				++fake_matches;
+				std::cout << "It's NOT A MATCH" << std::endl;
+			}
+
+			std::cout << std::endl;
+		}
+
+		old_char = current_chunk.front();
+		new_char = file.peek();
+
+		// Recalculate the current hash
+		hash_step(current_hash, phrase.length(), old_char, new_char);
+
+		// Pop the old char and append the next one;
+		current_chunk.erase(0, 1);
+		current_chunk += new_char;
+
+	} while (file.get() != EOF);
+
+	std::cout << "Fake Matches: " << fake_matches << '\n'
+	          << "Real Matches: " << real_matches
+	          << std::endl;
+
 	file.close();
 	return 0;
 }
